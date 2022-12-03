6773cbe0feaf79ca2f954b4e81410f6d81e571a8
lolomap <lolomap07@yandex.ru>
2022-12-04 01:30:51 +0300

    Resources page

--- conns_site/polls/views.py
+++ conns_site/polls/views.py
@@ -2,6 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.template.response import TemplateResponse
+from .models import Resource
+
 
 def index(request):
-	return HttpResponse('СЕТКА')
+	return TemplateResponse(request, 'index.html')
+
+
+def resources(request):
+	context_data = {'header': ['Автор', 'Проект', 'Ссылка'], 'rows': []}
+
+	resourcesObjs = Resource.objects.all()
+	for obj in resourcesObjs:
+		context_data['rows'].append(obj)
+
+	return TemplateResponse(request, 'resources.html', context=context_data)
