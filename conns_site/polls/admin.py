056b30510574ea186b4d0f1dfc454be0046a2aab
lolomap <lolomap07@yandex.ru>
2022-12-09 19:44:12 +0300

    Admin board

--- conns_site/polls/admin.py
+++ conns_site/polls/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+class ResourceAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'author', 'link')
+	list_display_links = ('id', 'name')
+	search_fields = ['author']
+
+admin.site.register(Resource, ResourceAdmin)
+admin.site.register(Article)
