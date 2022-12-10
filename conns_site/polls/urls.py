ee05636050c287dd011a32ed259e229feebdf143
lolomap <lolomap07@yandex.ru>
2022-12-10 21:31:29 +0300

    Articles

--- conns_site/polls/urls.py
+++ conns_site/polls/urls.py
@@ -1,7 +1,11 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('resources/', views.resources, name='resources'),
+	path('article/<article_id>/', views.article, name='article')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- conns_site/polls/views.py
+++ conns_site/polls/views.py
@@ -1,9 +1,10 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 from django.template.response import TemplateResponse
-from .models import Resource
+from .models import Resource, Article
 
 
 def index(request):
@@ -18,3 +19,12 @@ def resources(request):
 		context_data['rows'].append(obj)
 
 	return TemplateResponse(request, 'resources.html', context=context_data)
+
+def article(request, article_id):
+	aid = article_id.replace('-', ' ')
+	try:
+		articleObj = Article.objects.get(title=aid)
+		context_data = {'article': articleObj}
+		return TemplateResponse(request, 'article.html', context=context_data)
+	except ObjectDoesNotExist:
+		return HttpResponse(f'Статья {aid} не существует')
