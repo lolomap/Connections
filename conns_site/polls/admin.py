d5b75dd05f8b0a9b8cb9fcd293fdf7fa0ad7d9ca
lolomap <lolomap07@yandex.ru>
2022-12-10 22:38:38 +0300

    Pretty articles in admin board

--- conns_site/polls/admin.py
+++ conns_site/polls/admin.py
@@ -8,5 +8,11 @@ class ResourceAdmin(admin.ModelAdmin):
 	list_display_links = ('id', 'name')
 	search_fields = ['author']
 
+
+class ArticleAdmin(admin.ModelAdmin):
+	list_display = ('id', 'title', 'author', 'source', 'text')
+	list_display_links = ('id', 'title')
+	search_fields = ['author']
+
 admin.site.register(Resource, ResourceAdmin)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
