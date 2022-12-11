4bcc401efa22d689ac09acf996da591e830fa62e
lolomap <lolomap07@yandex.ru>
2022-12-11 11:57:09 +0300

    Additional comfortable urls

--- conns_site/polls/urls.py
+++ conns_site/polls/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('resources/', views.resources, name='resources'),
-	path('article/<article_id>/', views.article, name='article')
+	path('article/<article_id>/', views.article, name='article'),
+	path('<article_id>/', views.article, name='article')
 ]
 
 urlpatterns += staticfiles_urlpatterns()
